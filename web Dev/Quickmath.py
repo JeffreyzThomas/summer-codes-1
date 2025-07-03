db5588de187ab5b3d0176c4be2180b0110891a74
Jeffrey Thomas <Jeffrey.Thomas@hgs.hiddengeniusproject.org>
2025-07-03 13:42:15 -0700

    all

---
+++ web Dev/Quickmath.py
@@ -0,0 +1,20 @@
+1. What is a variable
+
+A varible is the name of the thing that hold data
+
+2. How many data types are there, and what are they
+
+There are four diffrent types, Strings, Floats, Booleans, and Integers
+
+3. What is the assignment operator, and what does it do
+
+An assignment operater is the = sign
+
+4. What does it mean to declare a variable
+
+To say what its saving or has
+
+5. What does it mean to assign a variable
+
+To give i a number
+ 
