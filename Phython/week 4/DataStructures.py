64275662064dfd85e0cda710b89af1a696639d1e
Jeffrey Thomas <Jeffrey.Thomas@hgs.hiddengeniusproject.org>
2025-07-07 13:43:40 -0700

    save

---
+++ Phython/week 4/DataStructures.py
@@ -0,0 +1,40 @@
+#list
+jeffreysSnacks = ["Kitkats", "Whatchamacllit", " Tea" , "carbination ", "Lemons"]
+jeffreysSnacks.append("snickers")
+jeffreysSnacks.sort()
+for snacks in jeffreysSnacks:
+     print(snacks)
+
+
+#Tuple
+EDplan = ("CCC", "Howard", "Stanford", "USC", "UC Davis")
+for ED in EDplan:
+  print(ED)
+
+
+  DCar = {"Brang": "Toyota",
+          "Model": "Supra",
+           "Year": 2014,
+           "Engine": "V8"
+           "Wheels": "16in"}
+DCar["color"] = "jet black carbine"
+
+
+
+#Set
+
+
+
+
+
+
+
+
+
+#Exit Ticket
+#Four built in data structures are, list, set, Dictinoray, and Tuple
+#When dats is mutable it means that you can make changes to it after you mkae the code. Immutable is the oppisite, you cant make changes.
+#A list is in an order
+#you add it with print
+#The list has to have the same varible name
+#They all need ()
