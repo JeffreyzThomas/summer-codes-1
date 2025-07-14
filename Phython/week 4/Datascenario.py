bb2c4d4623ef4518e40a8fe16f8f47c9eeab3282
Jeffrey Thomas <Jeffrey.Thomas@hgs.hiddengeniusproject.org>
2025-07-14 10:53:02 -0700

    hh

---
+++ Phython/week 4/Datascenario.py
@@ -0,0 +1,13 @@
+print("scenario #1 resturant" )
+print("Dictonary is the best for this becouse your able to pair food with prices. unlike a list")
+menu = {
+       "French toast": 11,
+       "Waffles": 14,
+       "Chicken + Waffles": 16
+}
+for key, item in menu.items():
+    print(key,": ", item)
+
+
+
+
